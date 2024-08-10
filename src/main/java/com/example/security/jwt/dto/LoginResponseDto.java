e3bcc50f1af10a5bc23ca075943454dce159864e
yashgirhe <girheyash@gmail.com>
2024-08-10 18:10:35 +0530

    added refresh token

---
+++ src/main/java/com/example/security/jwt/dto/LoginResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.security.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDto {
+
+    private Long id;
+    private String accessToken;
+    private String refreshToken;
+
+}
